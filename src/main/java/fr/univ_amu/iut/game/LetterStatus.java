fedbeb59e793f02a4dfcfab166d26f478f11e8d8
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-06-06 15:35:03 +0200

    Ajout du sujet en markdown

--- src/main/java/fr/univ_amu/iut/game/LetterStatus.java
+++ src/main/java/fr/univ_amu/iut/game/LetterStatus.java
@@ -1,9 +1,5 @@
 package fr.univ_amu.iut.game;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.css.PseudoClass;
-import javafx.scene.Node;
-
 public enum LetterStatus {
     EMPTY, UNLOCKED, WRONG, PRESENT, CORRECT
 }
